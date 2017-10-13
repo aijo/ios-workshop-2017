8c0dd4de1543a12a3f3911a4f8468287d67a5d23
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2017-10-13 12:09:50 +0700

    Transition to main screen with animation

--- ios/ViewController.swift
+++ ios/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var logoConstraint: NSLayoutConstraint!
+    
+    private let LOGO_DEFAULT_CONSTRAINT:CGFloat = 40
+    private let LOGO_HEIGHT:CGFloat = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        logoConstraint.constant = view.center.y-(LOGO_HEIGHT/2)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        logoConstraint.constant = LOGO_DEFAULT_CONSTRAINT
+        UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 3.0, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
