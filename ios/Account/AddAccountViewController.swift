32df61ee9e9c3f42edaa9c63248b888b2b7a3015
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2017-10-15 12:03:03 +0700

    Validate and add new account to account manager

--- ios/Account/AddAccountViewController.swift
+++ ios/Account/AddAccountViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 class AddAccountViewController: UIViewController {
 
     @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    
+    private let accountManager = AccountsManager.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Add Account"
         
-        // Do any additional setup after loading the view.
+        let dismissGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(dismissGesture)
+        
+        usernameTextfield.delegate = self
+        usernameTextfield.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
+        
+        addButton.isEnabled = false
+        addButton.setBackgroundColor(color: UIColor.init(rgba: "#2EB0FC"), forState: .normal)
+        addButton.setBackgroundColor(color: UIColor.init(rgba: "#EEEEEE"), forState: .disabled)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,7 +36,24 @@ class AddAccountViewController: UIViewController {
     }
     
     @IBAction func addButtonDidPressed(_ sender: Any) {
+        if let username = usernameTextfield.text {
+            accountManager.addAccount(username: username)
+        }
         self.navigationController?.popViewController(animated: true)
+    }
+    
+}
+
+@objc extension AddAccountViewController: UITextFieldDelegate {
+    
+    fileprivate func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    fileprivate func textFieldDidChange(_ textField: UITextField) {
+        if let username = usernameTextfield.text {
+            addButton.isEnabled = username.characters.count > 0
+        }
     }
     
 }
